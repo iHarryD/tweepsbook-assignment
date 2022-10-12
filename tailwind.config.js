ad2de3004daab025cae4d21b6ab6313640b1ffb2
iHarryD <anuraagvr555@gmail.com>
2022-10-12 23:05:17 +0530

    style: add theme to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./pages/**/*.{jsx,tsx}", "./components/**/*.{jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      accent: "#4ddaff",
+      "accent-text": "#000",
+      "primary-bg": "#202020",
+      "primary-text": "#fff",
+      "error-red": "#ff6767",
+      "success-green": "#53ff41",
+    },
   },
   plugins: [],
 };
